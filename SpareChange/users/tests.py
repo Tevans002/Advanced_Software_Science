13eecbaef387c7cead77bb5d27403bf04e606471
Jose Luis <jossseluuu@gmail.com>
2026-02-17 14:38:33 -0700

    All text functions with printed text

--- SpareChange/users/tests.py
+++ SpareChange/users/tests.py
@@ -8,6 +8,7 @@ class LoginSystemTest(TestCase):
         self.username = "mytestuser"
         self.password = "passwordset12345"
         self.user = User.objects.create_user(username=self.username, password=self.password)
+        print("User created: ", self.user)
 
     def testHomePageAnon(self):
         response = self.client.get(reverse('home'))
@@ -15,11 +16,13 @@ class LoginSystemTest(TestCase):
         self.assertContains(response, "You are not logged in.")
         self.assertContains(response, reverse('login'))
         self.assertContains(response, reverse('signup'))
+        print("Home page loaded")
 
     def testLoginPageLoads(self):
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/login.html')
+        print("Login page loaded")
 
     def testLoginSuccessful(self):
         response = self.client.post(reverse('login'), {
@@ -29,6 +32,7 @@ class LoginSystemTest(TestCase):
         self.assertRedirects(response, reverse('home'))
         self.assertContains(response, f"Hello, <strong>{self.username}</strong>!")
         self.assertTrue(response.context['user'].is_authenticated)
+        print("Login successful")
 
     def testLoginInvalidPassword(self):
         response = self.client.post(reverse('login'), {
@@ -37,17 +41,20 @@ class LoginSystemTest(TestCase):
         })  
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context['user'].is_authenticated)
+        print("Login invalid password")
 
     def testLogout(self):
         self.client.login(username=self.username, password=self.password)
         response = self.client.post(reverse('logout'), follow=True)
         self.assertRedirects(response, reverse('login'))
+        print("Logout successful")
 
     def testSignupPageLoads(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/signup.html')
-
+        print("Signup page loaded")
+        
     def testSignupSuccessful(self):
         new_username = "newuser"
         new_password = "newpassword123"
@@ -58,3 +65,4 @@ class LoginSystemTest(TestCase):
         }, follow=True)
         self.assertRedirects(response, reverse('login'))
         self.assertTrue(User.objects.filter(username=new_username).exists())
+        print("Signup successful")
