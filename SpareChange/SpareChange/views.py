5b80c3968e19351fd297cdf44431cf75a5d1db67
Samantha Ingraham <157062411+samantha-ingraham@users.noreply.github.com>
2026-03-10 17:20:31 -0600

    Setup models.py for users, as well as forms.py for users. added test for signup confirmation to ensure new fields are being stored in database correctly. adjusted settings.py tomake sure everything works as needed with new fields. setup static folder and css, js, and images folders for organization. adjusted templates to extend base correctly and added a basic nav bar. added new app for jobs for postings and such

--- SpareChange/SpareChange/views.py
+++ SpareChange/SpareChange/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import TemplateView, CreateView
-from django.contrib.auth.forms import UserCreationForm
+
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from users.forms import BaseUserCreationForm
+
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    form_class = BaseUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
