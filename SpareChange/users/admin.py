3c0296dc896915114105d54a9ce888fc1c38bfac
Samantha Ingraham <157062411+samantha-ingraham@users.noreply.github.com>
2026-03-12 16:46:14 -0600

    added admin pages to jobs and users. added sample data for jobs. deleted unused css folder, added some styling to job creation and added link for job creation to base.html. added template for job creation as well

--- SpareChange/users/admin.py
+++ SpareChange/users/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import base_user
 
-# Register your models here.
+
+@admin.register(base_user)
+class BaseUserAdmin(UserAdmin):
+    list_display = [
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_verified",
+        "is_staff",
+        "date_joined",
+    ]
+    list_filter = ["is_verified", "is_staff", "is_superuser", "is_active"]
+    search_fields = ["username", "email", "first_name", "last_name", "zipcode"]
+    ordering = ["-date_joined"]
+
+    # Add your custom fields to the edit page
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "user_bio",
+                    "zipcode",
+                    "profile_pic",
+                    "date_of_birth",
+                    "is_verified",
+                )
+            },
+        ),
+    )
+
+    # Add your custom fields to the create user page
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "user_bio",
+                    "zipcode",
+                    "profile_pic",
+                    "date_of_birth",
+                    "is_verified",
+                )
+            },
+        ),
+    )
